b980e59e8719459d2bf036d39357eea831cc71a5
Linus-Shyu <linusshyu@gmail.com>
2023-01-06 00:27:20 +0800

    first commit

---
+++ rust-learn9/target/debug/deps/rust_learn9-340b802d853da595.d
@@ -0,0 +1,5 @@
+/Users/faxinxu/Desktop/rust/rust-learn9/target/debug/deps/rust_learn9-340b802d853da595: src/main.rs
+
+/Users/faxinxu/Desktop/rust/rust-learn9/target/debug/deps/rust_learn9-340b802d853da595.d: src/main.rs
+
+src/main.rs:
---
+++ rust-learn9/target/debug/rust-learn9.d
@@ -0,0 +1,1 @@
+/Users/faxinxu/Desktop/rust/rust-learn9/target/debug/rust-learn9: /Users/faxinxu/Desktop/rust/rust-learn9/src/main.rs
