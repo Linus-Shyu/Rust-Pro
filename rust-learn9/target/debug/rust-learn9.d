5b78ac6b42f37c5b1e3e7bbb0c1e9967a820b0a9
Linus-Shyu <linusshyu@gmail.com>
2023-01-10 13:09:57 +0800

    Thirted commit

--- rust-learn9/target/debug/rust-learn9.d
+++ rust-learn9/target/debug/rust-learn9.d
@@ -1,1 +1,1 @@
-/Users/faxinxu/Desktop/rust/rust-learn9/target/debug/rust-learn9: /Users/faxinxu/Desktop/rust/rust-learn9/src/main.rs
+/Users/faxinxu/Desktop/rust\ pro/rust-learn9/target/debug/rust-learn9: /Users/faxinxu/Desktop/rust\ pro/rust-learn9/src/main.rs
